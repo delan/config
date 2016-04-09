8db33452cdfd55d4d23bd3f7577dd87d0c325b15
Delan Azabani <delan@azabani.com>
2016-04-10 04:54:48 +0800

    Add unbound(8)

--- root/backup.sh
+++ root/backup.sh
@@ -6,14 +6,17 @@ cd $out
 
 mkdir -p etc/rc.d etc/ssh root
 mkdir -p etc/openvpn var/run/openvpn
+mkdir -p var/unbound/etc
 cp /etc/myname etc
 cp /etc/sysctl.conf etc
 cp /etc/resolv.conf etc
 cp /etc/hostname.* etc
 cp /etc/pf.conf etc
+cp /etc/dhclient.conf etc
 cp /etc/dhcpd.conf etc
 cp /etc/dhcp6c.conf etc
 cp /etc/ntpd.conf etc
+cp /var/unbound/etc/unbound.conf var/unbound/etc
 cp /etc/rc.d/dhcp6c etc/rc.d
 cp /etc/rc.conf.local etc
 cp /etc/ssh/sshd_config etc/ssh
