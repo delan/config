cce7737023eeb293e4f8986d53635a3ccc14e94e
Delan Azabani <delan@azabani.com>
2023-11-27 12:17:27 +0800

    colo: fix acme challenge perms on opacus

--- nix/nixos/colo/opacus/install.sh
+++ nix/nixos/colo/opacus/install.sh
@@ -6,3 +6,6 @@ install -o root -g wheel -m 755 root/deploy-acme.sh /root/
 groupadd acme_ || :
 useradd -md /home/acme_ -s /bin/sh -g acme_ acme_ || :
 chmod 700 /home/acme_
+
+# for acme-dns.sh
+usermod -G _nsd acme_
