b5ef49123461b032ada7e14d39217f8706233b01
Delan Azabani <delan@azabani.com>
2024-02-03 12:48:35 +0800

    backups: update config and fix bugs in sync scripts

--- bin/sync-colo.sh
+++ bin/sync-colo.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env zsh
 set -euo shwordsplit
+# echo sync disabled; exit 1
 
-rsh='ssh delan@storage.home.daz.cat sudo'
+rsh='ssh delan@venus.home.daz.cat sudo'
 src_prefix=cuffs
 dest_prefix=ocean/dump/colo
 
@@ -13,15 +14,26 @@ for i in cuffs cuffs/{cache.nginx,nix}; do
   zfs destroy -v $i@$new
 done
 
-for i in home kate{,/vm0} opacus{,.www} root stratus.{vda,vdb}; do
+for i in home kate{,/vm0} opacus{,.www} root stratus.{vda,vdb} the6p4c{,/vm0}; do
   echo ">>> syncing $src_prefix/$i"
   unset old
-  zfs list -t bookmark -Ho name -d 1 $src_prefix/$i | sed 's/.*#//' | tac | while read -r j; do
-    if $rsh zfs list -t snapshot -Ho name $dest_prefix/$i@$j; then
+  set -- -t bookmark -Ho name -d 1 $src_prefix/$i
+  if [ $(zfs list "$@" | wc -l) -eq 0 ]; then
+    echo "fatal: no bookmarks in $src_prefix/$i"
+    exit 1
+  fi
+  zfs list "$@" | sed 's/.*#//' | tac | while read -r j; do
+    echo "=== checking for snapshot on destination: $dest_prefix/$i@$j"
+    # redirect to prevent ssh from stealing stdin
+    if $rsh zfs list -t snapshot -Ho name $dest_prefix/$i@$j < /dev/null; then
       old=$j
       break
     fi
   done
+  if [ -z "${old+set}" ]; then
+    echo "fatal: no bookmark has snapshot on destination: $src_prefix/$i"
+    exit 1
+  fi
   echo "=== old: $old"
   ~delan/bin/zfs-sync-snapshots --bookmark --delete all --delete-yes --rsh "$rsh" $dest_prefix $src_prefix/$i --incremental-source \#$old $new wet
 done
--- bin/sync-venus.sh
+++ bin/sync-venus.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env zsh
 set -euo shwordsplit
+# echo sync disabled; exit 1
 
-rsh='ssh delan@storage.home.daz.cat sudo'
+rsh=''
 src_prefix=cuffs
 dest_prefix=ocean/dump/venus
 
@@ -16,12 +17,23 @@ done
 for i in home root storage{,/data}; do
   echo ">>> syncing $src_prefix/$i"
   unset old
-  zfs list -t bookmark -Ho name -d 1 $src_prefix/$i | sed 's/.*#//' | tac | while read -r j; do
-    if $rsh zfs list -t snapshot -Ho name $dest_prefix/$i@$j; then
+  set -- -t bookmark -Ho name -d 1 $src_prefix/$i
+  if [ $(zfs list "$@" | wc -l) -eq 0 ]; then
+    echo "fatal: no bookmarks in $src_prefix/$i"
+    exit 1
+  fi
+  zfs list "$@" | sed 's/.*#//' | tac | while read -r j; do
+    echo "=== checking for snapshot on destination: $dest_prefix/$i@$j"
+    # redirect to prevent ssh from stealing stdin
+    if $rsh zfs list -t snapshot -Ho name $dest_prefix/$i@$j < /dev/null; then
       old=$j
       break
     fi
   done
+  if [ -z "${old+set}" ]; then
+    echo "fatal: no bookmark has snapshot on destination: $src_prefix/$i"
+    exit 1
+  fi
   echo "=== old: $old"
-  ~delan/bin/zfs-sync-snapshots --bookmark --delete old --delete-yes --rsh "$rsh" $dest_prefix $src_prefix/$i --incremental-source \#$old $new wet
+  ~delan/bin/zfs-sync-snapshots --bookmark --delete all --delete-yes --rsh "$rsh" $dest_prefix $src_prefix/$i --incremental-source \#$old $new wet
 done
