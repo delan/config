d5e863453977c5c7f29ae9209497f717b5795674
Delan Azabani <delan@azabani.com>
2024-07-02 12:15:05 +0800

    colo: prepare for new memories instance

--- nix/nixos/colo/configuration.nix
+++ nix/nixos/colo/configuration.nix
@@ -165,6 +165,7 @@
         "opacus.daz.cat"
         "stratus.daz.cat"
         "bucket.daz.cat"
+        "memories.daz.cat"
         "charming.daz.cat"
         "funny.computer.daz.cat"
         "go.daz.cat"
@@ -237,7 +238,7 @@
         };
       in {
         "\"\"" = {
-          locations."/" = {
+          locations."/disabled" = {
             return = "400";
           };
         };
@@ -246,6 +247,17 @@
         "opacus.daz.cat" = opacus // sslForce;
         "stratus.daz.cat" = stratus // sslForce;
         "bucket.daz.cat" = opacus // sslRelax;
+        "memories.daz.cat" = {
+          locations."/" = {
+            root = "/var/www/memories";
+            extraConfig = ''
+              # rewrite rules outside matching location do not apply!
+              location /peb/ {
+                return 403;
+              }
+            '';
+          };
+        } // sslForce;
         "charming.daz.cat" = opacus // sslForce;
         "funny.computer.daz.cat" = opacus // sslRelax;
         "go.daz.cat" = opacus // sslForce;
@@ -269,9 +281,6 @@
           locations."/" = {
             root = "/var/www/memories";
             extraConfig = ''
-              if ($remote_addr != "127.0.0.1") {
-                return 403;
-              }
               # rewrite rules outside matching location do not apply!
               location /peb/ {
                 auth_basic "/peb/";
@@ -280,6 +289,9 @@
                 rewrite ^/([^/]+)/([^/]+/.*)$ /$1/$2 break;
                 rewrite ^/([^/]+)/(meta[.]txt)$ /$1/$2 break;
                 rewrite ^/([^/]+)/(.*)$ /$1/index.html break;
+                if ($remote_addr != "127.0.0.1") {
+                  return 403;
+                }
               }
             '';
           };
