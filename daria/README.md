d9a06318d3ec83d245d994cfa9b4fd9586fc4c3d
Delan Azabani <delan@azabani.com>
2025-10-01 12:26:53 +0800

    dns: update home.daz.cat

--- daria/README.md
+++ daria/README.md
@@ -9,22 +9,13 @@ OpenBSD [home router config](https://www.azabani.com/2015/08/06/modern-openbsd-h
 - everything else replaced with **jane.daz.cat** (OPNsense) in 2021-09
 - config merged into [main config repo](https://github.com/delan/config.git) in 2025-09
 
-how to commit changes
-=====================
-
-```sh
-$ cd ~/daria.daz.cat && root/backup.sh && git add -N . && git commit -p
-```
-
 how to update rdns
 ==================
 
 ```sh
-$ cd /var/nsd/zones
-$ rg 2404:f780:8:3006: daz.cat.zone | fgrep -ve '@ ' -e 'ns ' | ./update-rdns.sh 2404.f780.8.3006.64.zone daz.cat dry
-$ rg 2403:580e:214:0: daz.cat.zone | ./update-rdns.sh 2403.580e.214.48.zone daz.cat dry
-$ rg 172.19.42. home.daz.cat.zone | ./update-rdns.sh 172.19.42.24.zone home.daz.cat dry
-$ rg fd36:09ef:4322:29ce: daz.cat.zone | ./update-rdns.sh fd36.09ef.4322.29ce.64.zone daz.cat dry
-$ rg 172.19.129. daz.cat.zone | ./update-rdns.sh 172.19.129.24.zone daz.cat dry
-$ rg 172.19.130. daz.cat.zone | ./update-rdns.sh 172.19.130.24.zone daz.cat dry
+$ cd /path/to/config/daria
+$ cd var/nsd/zones
+$ ./update-rdns.sh dry
+$ ./update-rdns.sh wet
+$ git diff
 ```
--- daria/var/nsd/zones/update-rdns.sh
+++ daria/var/nsd/zones/update-rdns.sh
@@ -1,27 +1,35 @@
 #!/usr/bin/env zsh
-# usage: rg 192.0.2. path/to/forward/zone | update-rdns6.sh <path/to/reverse/zone> <suffix.daz.cat> <dry|wet>
-set -eu
+# usage: update-rdns.sh <dry|wet>
+set -euo pipefail
 
-zone=$1
-suffix=$2
-drywet=$3
+drywet=$1
 
-< "$zone" head -1 | cut -d\( -f2 | read -r oldserial _
-newserial=$(date -u +\%Y\%m\%d00)
-while [ $newserial -le $oldserial ]; do
-  newserial=$((newserial+1))
-done
+if [ "$drywet" = dry ]; then echo '>>> 2404.f780.8.3006.64.zone' >&2; fi
+rg 2404:f780:8:3006: daz.cat.zone | fgrep -ve '@ ' -e 'ns ' \
+| ./update-rdns-for-zone.sh 2404.f780.8.3006.64.zone daz.cat "$drywet"
+if [ "$drywet" = dry ]; then echo >&2; fi
 
-case "$drywet" in
-(dry) out=/dev/stdout ;;
-(wet) out=$zone ;;
-(*) >&2 echo 'fatal: $2 must be dry or wet'; exit 1 ;;
-esac
+if [ "$drywet" = dry ]; then echo '>>> 2403.580e.214.48.zone' >&2; fi
+rg 2403:580e:214:0: daz.cat.zone \
+| ./update-rdns-for-zone.sh 2403.580e.214.48.zone daz.cat "$drywet"
+if [ "$drywet" = dry ]; then echo >&2; fi
 
-> "$out" echo "@ 60 IN SOA ns.daz.cat. delan.azabani.com. ( $newserial 600 60 1814400 60 )"
->> "$out" echo "@ 60 IN NS ns.daz.cat."
-while read -r name _ _ _ ip; do
-  # bore encode/decode so we don’t actually send the query
-  bore --encode -x "$ip" | bore --decode | rg -A1 '^;; question section$' | tail -1 | read -r reverse _
-  >> "$out" echo "$reverse 60 IN PTR $name.$suffix."
-done
+if [ "$drywet" = dry ]; then echo '>>> 172.19.42.24.zone' >&2; fi
+rg 172.19.42. home.daz.cat.zone \
+| ./update-rdns-for-zone.sh 172.19.42.24.zone home.daz.cat "$drywet"
+if [ "$drywet" = dry ]; then echo >&2; fi
+
+if [ "$drywet" = dry ]; then echo '>>> fd36.09ef.4322.29ce.64.zone' >&2; fi
+rg fd36:09ef:4322:29ce: daz.cat.zone \
+| ./update-rdns-for-zone.sh fd36.09ef.4322.29ce.64.zone daz.cat "$drywet"
+if [ "$drywet" = dry ]; then echo >&2; fi
+
+if [ "$drywet" = dry ]; then echo '>>> 172.19.129.24.zone' >&2; fi
+rg 172.19.129. daz.cat.zone \
+| ./update-rdns-for-zone.sh 172.19.129.24.zone daz.cat "$drywet"
+if [ "$drywet" = dry ]; then echo >&2; fi
+
+if [ "$drywet" = dry ]; then echo '>>> 172.19.130.24.zone' >&2; fi
+rg 172.19.130. daz.cat.zone \
+| ./update-rdns-for-zone.sh 172.19.130.24.zone daz.cat "$drywet"
+if [ "$drywet" = dry ]; then echo >&2; fi
