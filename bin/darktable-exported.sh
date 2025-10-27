64aacfbc6bfaaa5548692d2ab893135e6f08b668
Delan Azabani <delan@azabani.com>
2025-10-27 14:19:16 +0800

    update darktable dir

--- bin/darktable-exported.sh
+++ bin/darktable-exported.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env zsh
 set -eu
-SHUPPY_DARKTABLE_DIR=${SHUPPY_DARKTABLE_DIR-$HOME/darktable}
+SHUPPY_DARKTABLE_DIR=${SHUPPY_DARKTABLE_DIR-/cuffs/darktable}
 for i; do
     echo $SHUPPY_DARKTABLE_DIR/${i%%_*}_*/darktable_exported/$i
 done
