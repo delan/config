3a5a0a01e7eda002292542392b4b9f10d4f4968c
Delan Azabani <delan@azabani.com>
2026-01-03 18:15:00 +0800

    jupiter: sync darktable dataset

--- bin/sync-jupiter.sh
+++ bin/sync-jupiter.sh
@@ -3,8 +3,6 @@ set -euo shwordsplit
 # echo sync disabled; exit 1
 
 rsh='ssh delan@venus.home.daz.cat sudo'
-src_prefix=cuffs
-dest_prefix=ocean/dump/jupiter
 
 new=$(date -u +\%FT\%RZ)
 echo ">>> new: $new"
@@ -14,26 +12,34 @@ zfs list -Ho name -r cuffs | rg '^cuffs/(base|build|ci|nix)(/|$)' | while read -
   zfs destroy -v $i@$new
 done
 
-for i in code home root; do
-  echo ">>> syncing $src_prefix/$i"
+src_dest_pairs=(
+  cuffs/code ocean/dump/jupiter/code
+  cuffs/home ocean/dump/jupiter/home
+  cuffs/root ocean/dump/jupiter/root
+  cuffs/darktable ocean/private/shuppy/darktable
+)
+while [ ${#src_dest_pairs} -gt 0 ]; do
+  src_dataset=${src_dest_pairs[1]}; shift src_dest_pairs
+  dest_dataset=${src_dest_pairs[1]}; shift src_dest_pairs
+  echo ">>> syncing $src_dataset → $dest_dataset"
   unset old
-  set -- -t bookmark -Ho name -d 1 $src_prefix/$i
+  set -- -t bookmark -Ho name -d 1 $src_dataset
   if [ $(zfs list "$@" | wc -l) -eq 0 ]; then
-    echo "fatal: no bookmarks in $src_prefix/$i"
+    echo "fatal: no bookmarks in $src_dataset"
     exit 1
   fi
   zfs list "$@" | sed 's/.*#//' | tac | while read -r j; do
-    echo "=== checking for snapshot on destination: $dest_prefix/$i@$j"
+    echo "=== checking for snapshot on destination: $dest_dataset@$j"
     # redirect to prevent ssh from stealing stdin
-    if $rsh zfs list -t snapshot -Ho name $dest_prefix/$i@$j < /dev/null; then
+    if $rsh zfs list -t snapshot -Ho name $dest_dataset@$j < /dev/null; then
       old=$j
       break
     fi
   done
   if [ -z "${old+set}" ]; then
-    echo "fatal: no bookmark has snapshot on destination: $src_prefix/$i"
+    echo "fatal: no bookmark has snapshot on destination: $src_dataset"
     exit 1
   fi
   echo "=== old: $old"
-  zfs-sync-snapshots --bookmark --delete this --i-am-a-cron-job-fuck-me-up-and-delete-without-asking --rsh "$rsh" $dest_prefix $src_prefix/$i --incremental-source \#$old $new wet
+  zfs-sync-snapshots --bookmark --delete this --i-am-a-cron-job-fuck-me-up-and-delete-without-asking --rsh "$rsh" $dest_dataset $src_dataset --incremental-source \#$old $new wet
 done
