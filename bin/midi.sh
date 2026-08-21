0f6aafcc25d15de16e5912359a153edb2e6a6006
delan azabani <delan@azabani.com>
2026-08-21 17:51:54 +0800

    NixOS: fix nix eval syntax in midi.sh

--- bin/midi.sh
+++ bin/midi.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 set -eu
-exec nohup timidity -iA -B2,8 -Os1l -s 44100 -x "soundfont $(nix eval --raw nixos.soundfont-fluid.outPath)/share/soundfonts/FluidR3_GM2-2.sf2 order=1" > $(mktemp /tmp/midi..XXXXXXXXXXXXX | tee /dev/stderr) &
+exec nohup timidity -iA -B2,8 -Os1l -s 44100 -x "soundfont $(nix eval --raw nixpkgs\#soundfont-fluid)/share/soundfonts/FluidR3_GM2-2.sf2 order=1" > $(mktemp /tmp/midi..XXXXXXXXXXXXX | tee /dev/stderr) &
